b60beef61bb6abe5bf16c4070b5a9522aca67a52
web3dev00 <development@dechains.com>
2024-02-21 17:31:45 +0500

    init

---
+++ components/header.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import Link from 'next/link'
+
+import { cn } from '@/lib/utils'
+import { buttonVariants } from '@/components/ui/button'
+import { IconGitHub } from '@/components/ui/icons'
+
+export function Header() {
+  return (
+    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
+      <Link className="flex items-center space-x-2" href="/">
+        <span className="text-xl font-bold tracking-tight text-foreground">
+          Harry Potter GPT
+        </span>
+      </Link>
+      <div className="flex items-center justify-end space-x-2">
+        <a
+          target="_blank"
+          href="https://github.com/vercel/nextjs-ai-chatbot/"
+          rel="noopener noreferrer"
+          className={cn(buttonVariants({ variant: 'outline' }))}
+        >
+          <IconGitHub />
+          <span className="hidden ml-2 md:flex">View On GitHub</span>
+        </a>
+      </div>
+    </header>
+  )
+}
