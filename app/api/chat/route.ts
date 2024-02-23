bb881dad845f2b3c85b644889faf08191528154c
web3dev00 <development@dechains.com>
2024-02-23 18:17:19 +0500

    chore: langchain integrated for pdf contexts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,9 +1,38 @@
 import { kv } from '@vercel/kv'
+import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai'
+import { RunnableSequence, RunnableMap } from '@langchain/core/runnables'
+import { ChatPromptTemplate } from '@langchain/core/prompts'
+import { StringOutputParser } from '@langchain/core/output_parsers'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import OpenAI from 'openai'
 
 import { auth } from '@/auth'
 import { nanoid } from '@/lib/utils'
+import { PineconeStore } from '@langchain/pinecone'
+import { Pinecone } from '@pinecone-database/pinecone'
+import { Document } from 'langchain/document'
+
+const convertDocsToString = (documents: Document[]) => {
+  return documents
+    .map(doc => {
+      return `<doc>\n${doc.pageContent}\n</doc>`
+    })
+    .join('\n')
+}
+
+export const TEMPLATE_STRING = `
+    You are an experienced researcher, expert at interpreting and answering questions based on the provided sources.
+    Using the below provided context, answer the user's question to the best of your ability using only the resources provided.
+    Be verbose!
+
+    <context>
+        {context}
+    </context>
+
+    Now, answer the following question.
+
+    {question}
+`
 
 export const runtime = 'edge'
 
@@ -11,48 +40,86 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 })
 
+const model = new ChatOpenAI({
+  modelName: 'gpt-3.5-turbo',
+  streaming: true
+})
+const pinecone = new Pinecone()
+
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME!)
+
 export async function POST(req: Request) {
   const json = await req.json()
   const { messages, previewToken } = json
-
+  console.log('messages =>', messages)
   if (previewToken) {
     openai.apiKey = previewToken
   }
 
-  const res = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages,
-    temperature: 0.7,
-    stream: true
+  const vectorstore = await PineconeStore.fromExistingIndex(
+    new OpenAIEmbeddings(),
+    { pineconeIndex }
+  )
+
+  const retriever = vectorstore.asRetriever()
+
+  const documentRetrievalChain = RunnableSequence.from([
+    input => input.question,
+    retriever,
+    convertDocsToString
+  ])
+
+  const answerGenerationPrompt =
+    ChatPromptTemplate.fromTemplate(TEMPLATE_STRING)
+
+  const retrievalChain = RunnableSequence.from([
+    {
+      context: documentRetrievalChain,
+      question: input => input.question
+    },
+    answerGenerationPrompt,
+    model,
+    new StringOutputParser()
+  ])
+
+  // const res = await openai.chat.completions.create({
+  //   model: 'gpt-3.5-turbo',
+  //   messages,
+  //   temperature: 0.7,
+  //   stream: true
+  // })
+
+  const res = await retrievalChain.stream({
+    question: messages[messages.length - 1].content
   })
 
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        // userId,
-        createdAt,
-        path,
-        messages: [
-          ...messages,
-          {
-            content: completion,
-            role: 'assistant'
-          }
-        ]
-      }
-      // await kv.hmset(`chat:${id}`, payload)
-      // await kv.zadd(`user:chat:${userId}`, {
-      //   score: createdAt,
-      //   member: `chat:${id}`
-      // })
-    }
-  })
+  // const stream = OpenAIStream(res, {
+  //   async onCompletion(completion) {
+  //     const title = json.messages[0].content.substring(0, 100)
+  //     const id = json.id ?? nanoid()
+  //     const createdAt = Date.now()
+  //     const path = `/chat/${id}`
+  //     const payload = {
+  //       id,
+  //       title,
+  //       // userId,
+  //       createdAt,
+  //       path,
+  //       messages: [
+  //         ...messages,
+  //         {
+  //           content: completion,
+  //           role: 'assistant'
+  //         }
+  //       ]
+  //     }
+  //     // await kv.hmset(`chat:${id}`, payload)
+  //     // await kv.zadd(`user:chat:${userId}`, {
+  //     //   score: createdAt,
+  //     //   member: `chat:${id}`
+  //     // })
+  //   }
+  // })
 
-  return new StreamingTextResponse(stream)
+  return new StreamingTextResponse(res)
 }
--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -1,24 +1,30 @@
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 import { OpenAIEmbeddings } from '@langchain/openai'
-import { PineconeStore } from 'langchain/vectorstores/pinecone'
 // import { pinecone } from '@/utils/pinecone-client';
 import { CustomPDFLoader } from '@/utils/customPDFLoader'
 // import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory'
 import fs from 'fs'
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+import { Pinecone } from '@pinecone-database/pinecone'
+import { PineconeStore } from '@langchain/pinecone'
 
 /* Name of directory to retrieve your files from */
-const filePath = 'books'
+const filePath = 'books/Harry Potter - Book 1 - The Sorcerers Stone.pdf'
 
 export const run = async () => {
   try {
     /*load raw docs from the all files in the directory */
-    const directoryLoader = new DirectoryLoader(filePath, {
-      '.pdf': path => new CustomPDFLoader(path)
-    })
+    // const directoryLoader = new DirectoryLoader(filePath, {
+    //   '.pdf': path => new CustomPDFLoader(path)
+    // })
 
-    // const loader = new PDFLoader(filePath);
-    const rawDocs = await directoryLoader.load()
+    const loader = new PDFLoader(filePath)
+    // const rawDocs = await directoryLoader.load()
+
+    const rawDocs = await loader.load()
+
+    // console.log('rawDocs', rawDocs.length)
 
     /* Split text into chunks */
     const textSplitter = new RecursiveCharacterTextSplitter({
@@ -27,20 +33,34 @@ export const run = async () => {
     })
 
     const docs = await textSplitter.splitDocuments(rawDocs)
-    console.log('split docs', docs[0].metadata.loc)
 
-    const embeddings = new OpenAIEmbeddings({
-      modelName: 'text-embedding-3-small'
-    })
+    // const pinecone = new Pinecone()
 
-    const vectors = await embeddings.embedDocuments(
-      docs.slice(0, 10).map(d => d.pageContent)
-    )
+    // const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME!)
 
-    console.log('vectors', vectors)
+    // const embeddings = new OpenAIEmbeddings()
+
+    // const vectorStore = await PineconeStore.fromDocuments(docs, embeddings, {
+    //   pineconeIndex,
+    //   maxConcurrency: 5
+    // })
+
+    // console.log('vectorStore', vectorStore)
+
+    // console.log('split docs', docs[0].metadata.loc)
+
+    // const embeddings = new OpenAIEmbeddings({
+    //   modelName: 'text-embedding-3-small'
+    // })
+
+    // const vectors = await embeddings.embedDocuments(
+    //   docs.slice(0, 10).map(d => d.pageContent)
+    // )
+
+    // console.log('vectors', vectors)
 
     // save the vectors to a file.
-    fs.writeFileSync('vectors.json', JSON.stringify(vectors))
+    // fs.writeFileSync('vectors.json', JSON.stringify(vectors))
 
     // console.log('creating vector store...');
     // /*create and store the embeddings in the vectorStore*/
