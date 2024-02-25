605183c061ff118ee8ee8ed5630000f4d00c84e8
Arsalan-Habib <syedarsalanhabib@gmail.com>
2024-02-26 02:35:48 +0500

    feat: add conversation history awareness.

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,16 +1,13 @@
-import { kv } from '@vercel/kv'
 import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai'
-import { RunnableSequence, RunnableMap } from '@langchain/core/runnables'
+import { RunnableSequence } from '@langchain/core/runnables'
 import { ChatPromptTemplate } from '@langchain/core/prompts'
 import { StringOutputParser } from '@langchain/core/output_parsers'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-import OpenAI from 'openai'
-
-import { auth } from '@/auth'
-import { nanoid } from '@/lib/utils'
 import { PineconeStore } from '@langchain/pinecone'
 import { Pinecone } from '@pinecone-database/pinecone'
 import { Document } from 'langchain/document'
+import { StreamingTextResponse } from 'ai'
+
+export const runtime = 'edge'
 
 const convertDocsToString = (documents: Document[]) => {
   return documents
@@ -20,10 +17,10 @@ const convertDocsToString = (documents: Document[]) => {
     .join('\n')
 }
 
-export const TEMPLATE_STRING = `
-    You are an experienced researcher, expert at interpreting and answering questions based on the provided sources.
+const answerGenerationPrompt = ChatPromptTemplate.fromTemplate(`
+    You are a helpful bank teller agent, skilled in assisting customers with their banking needs and inquiries.
     Using the below provided context, answer the user's question to the best of your ability using only the resources provided.
-    Be verbose!
+    Be informative and courteous! 
 
     <context>
         {context}
@@ -32,94 +29,90 @@ export const TEMPLATE_STRING = `
     Now, answer the following question.
 
     {question}
-`
+`)
 
-export const runtime = 'edge'
+const historyAwarePrompt = ChatPromptTemplate.fromTemplate(`
+Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-})
+<chat_history>
+  {chat_history}
+</chat_history>
+
+Follow Up Input: {question}
+Standalone question:
+`)
 
 const model = new ChatOpenAI({
   modelName: 'gpt-3.5-turbo',
-  streaming: true
+  streaming: true,
+  temperature: 0
 })
 const pinecone = new Pinecone()
 
 const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME!)
 
 export async function POST(req: Request) {
-  const json = await req.json()
-  const { messages, previewToken } = json
-  console.log('messages =>', messages)
-  if (previewToken) {
-    openai.apiKey = previewToken
+  try {
+    const json = await req.json()
+    const { messages } = json
+
+    const question = messages[messages.length - 1].content
+
+    const formattedChatHistory = messages
+      // remove the last message which is the question.
+      .slice(0, messages.length - 1)
+
+      .map(
+        (msg: { role: string; content: string }) =>
+          `${msg.role}: ${msg.content}`
+      )
+      .join('\n')
+
+    console.log('Question=>', question)
+
+    const vectorStore = await PineconeStore.fromExistingIndex(
+      new OpenAIEmbeddings({
+        modelName: 'text-embedding-3-small'
+      }),
+      { pineconeIndex, namespace: process.env.PINECONE_NAME_SPACE! }
+    )
+
+    const retriever = vectorStore.asRetriever()
+
+    const historyAwareQuestionGenerationChain = RunnableSequence.from([
+      () => ({
+        chat_history: formattedChatHistory,
+        question: question
+      }),
+      historyAwarePrompt,
+      model,
+      new StringOutputParser()
+    ])
+
+    const documentRetrievalChain = RunnableSequence.from([
+      historyAwareQuestionGenerationChain,
+      retriever,
+      convertDocsToString
+    ])
+
+    const conversationalChain = RunnableSequence.from([
+      {
+        context: documentRetrievalChain,
+        question: input => input.question
+      },
+      answerGenerationPrompt,
+      model,
+      new StringOutputParser()
+    ])
+
+    const res = await conversationalChain.stream({
+      question
+    })
+
+    return new StreamingTextResponse(res)
+  } catch (error) {
+    console.error('error', error)
+    console.trace(error)
+    return new Response('Failed to process request', { status: 500 })
   }
-
-  const vectorstore = await PineconeStore.fromExistingIndex(
-    new OpenAIEmbeddings(),
-    { pineconeIndex }
-  )
-
-  const retriever = vectorstore.asRetriever()
-
-  const documentRetrievalChain = RunnableSequence.from([
-    input => input.question,
-    retriever,
-    convertDocsToString
-  ])
-
-  const answerGenerationPrompt =
-    ChatPromptTemplate.fromTemplate(TEMPLATE_STRING)
-
-  const retrievalChain = RunnableSequence.from([
-    {
-      context: documentRetrievalChain,
-      question: input => input.question
-    },
-    answerGenerationPrompt,
-    model,
-    new StringOutputParser()
-  ])
-
-  // const res = await openai.chat.completions.create({
-  //   model: 'gpt-3.5-turbo',
-  //   messages,
-  //   temperature: 0.7,
-  //   stream: true
-  // })
-
-  const res = await retrievalChain.stream({
-    question: messages[messages.length - 1].content
-  })
-
-  // const stream = OpenAIStream(res, {
-  //   async onCompletion(completion) {
-  //     const title = json.messages[0].content.substring(0, 100)
-  //     const id = json.id ?? nanoid()
-  //     const createdAt = Date.now()
-  //     const path = `/chat/${id}`
-  //     const payload = {
-  //       id,
-  //       title,
-  //       // userId,
-  //       createdAt,
-  //       path,
-  //       messages: [
-  //         ...messages,
-  //         {
-  //           content: completion,
-  //           role: 'assistant'
-  //         }
-  //       ]
-  //     }
-  //     // await kv.hmset(`chat:${id}`, payload)
-  //     // await kv.zadd(`user:chat:${userId}`, {
-  //     //   score: createdAt,
-  //     //   member: `chat:${id}`
-  //     // })
-  //   }
-  // })
-
-  return new StreamingTextResponse(res)
 }
