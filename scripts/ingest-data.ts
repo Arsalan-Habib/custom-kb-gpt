57cb43364ab9d2649aed69472c75c438435a7a6a
Arsalan-Habib <syedarsalanhabib@gmail.com>
2024-02-26 02:34:33 +0500

    refactor: cleanup ingestion script, add new script for memory vector store

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -1,30 +1,18 @@
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 import { OpenAIEmbeddings } from '@langchain/openai'
-// import { pinecone } from '@/utils/pinecone-client';
-import { CustomPDFLoader } from '@/utils/customPDFLoader'
-// import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
-import { DirectoryLoader } from 'langchain/document_loaders/fs/directory'
-import fs from 'fs'
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 import { Pinecone } from '@pinecone-database/pinecone'
 import { PineconeStore } from '@langchain/pinecone'
 
-/* Name of directory to retrieve your files from */
-const filePath = 'books/Harry Potter - Book 1 - The Sorcerers Stone.pdf'
+const filePath = 'docs/chase-account-guide.pdf'
 
-export const run = async () => {
+export const populatePineconeVectorStore = async (filePath: string) => {
   try {
-    /*load raw docs from the all files in the directory */
-    // const directoryLoader = new DirectoryLoader(filePath, {
-    //   '.pdf': path => new CustomPDFLoader(path)
-    // })
-
+    console.log('Initializing document ingestion!')
     const loader = new PDFLoader(filePath)
-    // const rawDocs = await directoryLoader.load()
 
     const rawDocs = await loader.load()
-
-    // console.log('rawDocs', rawDocs.length)
 
     /* Split text into chunks */
     const textSplitter = new RecursiveCharacterTextSplitter({
@@ -32,53 +20,62 @@ export const run = async () => {
       chunkOverlap: 200
     })
 
-    const docs = await textSplitter.splitDocuments(rawDocs)
+    const splitDocuments = await textSplitter.splitDocuments(rawDocs)
 
-    // const pinecone = new Pinecone()
+    const pinecone = new Pinecone()
 
-    // const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME!)
+    const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME!)
 
-    // const embeddings = new OpenAIEmbeddings()
+    const embeddings = new OpenAIEmbeddings({
+      modelName: 'text-embedding-3-small'
+    })
 
-    // const vectorStore = await PineconeStore.fromDocuments(docs, embeddings, {
-    //   pineconeIndex,
-    //   maxConcurrency: 5
-    // })
+    const vectorStore = await PineconeStore.fromDocuments(
+      splitDocuments,
+      embeddings,
+      {
+        pineconeIndex,
+        namespace: process.env.PINECONE_NAME_SPACE!,
+        textKey: 'text'
+      }
+    )
 
-    // console.log('vectorStore', vectorStore)
-
-    // console.log('split docs', docs[0].metadata.loc)
-
-    // const embeddings = new OpenAIEmbeddings({
-    //   modelName: 'text-embedding-3-small'
-    // })
-
-    // const vectors = await embeddings.embedDocuments(
-    //   docs.slice(0, 10).map(d => d.pageContent)
-    // )
-
-    // console.log('vectors', vectors)
-
-    // save the vectors to a file.
-    // fs.writeFileSync('vectors.json', JSON.stringify(vectors))
-
-    // console.log('creating vector store...');
-    // /*create and store the embeddings in the vectorStore*/
-    // const embeddings = new OpenAIEmbeddings();
-    // const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
-
-    // //embed the PDF documents
-    // await PineconeStore.fromDocuments(docs, embeddings, {
-    //   pineconeIndex: index,
-    //   namespace: PINECONE_NAME_SPACE,
-    //   textKey: 'text',
-    // });
+    return vectorStore
   } catch (error) {
     console.log('error', error)
-    throw new Error('Failed to ingest your data')
+    throw new Error('Failed to initialize pinecone store')
+  }
+}
+
+export const initializeMemoryVectorStore = async (filePath: string) => {
+  try {
+    const loader = new PDFLoader(filePath)
+
+    const rawDocs = await loader.load()
+
+    /* Split text into chunks */
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200
+    })
+
+    const splitDocuments = await textSplitter.splitDocuments(rawDocs)
+
+    const embeddings = new OpenAIEmbeddings({
+      modelName: 'text-embedding-3-small'
+    })
+
+    const vectorStore = new MemoryVectorStore(embeddings)
+
+    await vectorStore.addDocuments(splitDocuments)
+
+    return vectorStore
+  } catch (error) {
+    console.log('error', error)
+    throw new Error('Failed to initialize memory vector store')
   }
 }
 ;(async () => {
-  await run()
+  await populatePineconeVectorStore(filePath)
   console.log('ingestion complete')
 })()
